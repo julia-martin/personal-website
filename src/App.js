b787b8c020a5c371c5633c8dfdf9a7f6d93c945d
Julia <juliadmartin720@gmail.com>
2021-12-28 15:29:53 -0600

    Change back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import SimilartPage from "./components/SimilartPage";
 import "./App.css";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage/>} />
         <Route path="/similart" element={<SimilartPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
