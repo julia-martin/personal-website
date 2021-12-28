aac18959c8e4621f5187548d4a0f883fb528e600
Julia <juliadmartin720@gmail.com>
2021-12-28 15:14:10 -0600

    Change BrowserRouter to HashRouter for Netlify

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import SimilartPage from "./components/SimilartPage";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<HomePage/>} />
         <Route path="/similart" element={<SimilartPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
