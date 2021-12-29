fe0287e2647d0a8f5b25d6b066fa28e4a6426bec
Julia <juliadmartin720@gmail.com>
2021-12-29 12:46:13 -0600

    Add animation to Similart title

--- src/components/SimilartPage.js
+++ src/components/SimilartPage.js
@@ -95,9 +95,10 @@ const Title = styled.h1`
 
   @media (prefers-reduced-motion: no-preference) {
     will-change: transform;
+    animation: ${ANIMATIONS.slideDown} 700ms both;
+
     &:hover {
       animation: ${ANIMATIONS.spaceOut} 500ms 2 alternate;
-
     }
   }
 
